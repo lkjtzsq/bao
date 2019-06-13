50597cf7d220db913087a02c431ab7cf5aa8c017
lkjtzsq <879932397@qq.com>
2019-06-13 18:31:06 +0800

    33

--- src/pages/meeting/meeting.js
+++ src/pages/meeting/meeting.js
@@ -28,11 +28,11 @@ loader.define(function(){
         ];
         $('body').on("click",".meeting-item",function(){
             var index=$(this).index();
-            var item=meetingArr[index];
             $('.ts-head h1').html($(this).find('.meeting-title').html());
             $('.ts-desc').html($(this).find('.p1').html());
             $('.ts-count span').html($(this).find('.p2').html());
             $('.time-chose-bg,.time-chose').show();
+            getTime(index);
         })
         $('.time-chose-bg').click(function(){
             $(this).hide();
@@ -51,6 +51,7 @@ loader.define(function(){
             $(this).hide();
             $('.view-pic-bg').hide();
         })
+        $('.meeting-contain').height($(window).height()-123);
         // 虚拟storage
         var storage=bui.storage();
         storage.set("token","2bc4bc9fcb24b2903d61e7b7921409d3");
@@ -71,20 +72,68 @@ loader.define(function(){
                 "token":"2bc4bc9fcb24b2903d61e7b7921409d3"
             }
         }).then(function(data){
-            console.log(data);
             var data=data.data.data;
             var str='';
             $(data).each(function(i,v){
+                var spanStr='';
                 var name=v.name;
                 var meeting_desc=v.meeting_desc;
                 var pnumber=v.pnumber;
                 var address=v.address;
+                var books=v.books;
+                $(books).each(function(k,z){
+                    if(z==1){
+                        spanStr+='<span style="background:#f5dcca"></span>';
+                    }else{
+                        spanStr+='<span></span>';
+                    }
+                });
                 var thumbnail=v.thumbnail[0] || "/images/pic-default.png";
-                str+='<div class="meeting-item"><div class="meeting-room"><div class="meeting-left"><h1 class="meeting-title">'+v.name+'</h1><p class="meeting-cr p1">'+v.devices+'</p><p class="meeting-cr p2">'+v.pnumber+'人</p><p class="meeting-cr p3">'+v.address+'</p></div><div class="meeting-right"><img src="'+v.thumbnail+'"></div></div><div class="meeting-progress"><div class="progress-bar"><span></span></div><div class="progress-count"><span>07</span><span>08</span><span>09</span><span>10</span><span>11</span><span>12</span><span>13</span><span>14</span><span>15</span><span>16</span><span>17</span><span>18</span><span>19</span><span>20</span><span>21</span><span>22</span><span>23</span></div></div></div>';
+                str+='<div class="meeting-item"><div class="meeting-room"><div class="meeting-left"><h1 class="meeting-title">'+v.name+'</h1><p class="meeting-cr p1">'+v.devices+'</p><p class="meeting-cr p2">'+v.pnumber+'人</p><p class="meeting-cr p3">'+v.address+'</p></div><div class="meeting-right"><img src="'+v.thumbnail+'"></div></div><div class="meeting-progress"><div class="progress-bar">'+spanStr+'</div><div class="progress-count"><span>07</span><span>08</span><span>09</span><span>10</span><span>11</span><span>12</span><span>13</span><span>14</span><span>15</span><span>16</span><span>17</span><span>18</span><span>19</span><span>20</span><span>21</span><span>22</span><span>23</span></div></div></div>';
             });
             $(str).appendTo($('.meeting-contain'));            
-        },function(err){console.log(err)});
+        });
        }
        getList();
+       $('.ts-group input').not(':checked').change(function(){
+            var inputCheckedLen=$('.ts-group input:checked').length;
+            if(inputCheckedLen>0){
+                $('.ts-btn').addClass('on');
+            }else{
+                $('.ts-btn').removeClass('on');
+            }
+       });
+        function getTime(index){
+             // ajax请求
+            var apiUrl="";
+            var curDate=new Date();
+            var year=curDate.getFullYear();
+            var month=curDate.getMonth()+1;
+            var day=curDate.getDate();
+            var date=year+'-'+month+'-'+day;
+            // console.log(date);
+            $('.ts-group').find('input[type="checkbox"]').removeAttr('disabled').removeAttr("checked");;
+            $('.ts-group').find('label').removeClass('disabled');
+             $('.ts-btn').removeClass('on');
+            bui.ajax({
+                url:apiUrl+"api/meeting",
+                type:"get",
+                data:{
+                    "date":date,
+                    "token":"2bc4bc9fcb24b2903d61e7b7921409d3"
+                }
+            }).then(function(data){
+                console.log(data);
+                var books=data.data.data[index].books;
+                console.log(books);
+                
+                $(books).each(function(i,v){
+                    if(v==1){
+                        $('.ts-group').eq(i).find('input[type="checkbox"]').attr('disabled','true');
+                        $('.ts-group').eq(i).find('label').addClass('disabled');
+                    }
+                });
+            });
+       }
     });
 })
